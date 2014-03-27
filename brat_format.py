c50ddbec752063a0a1ddc9291886d90c0e5a001d
EliasAamot <eliasaa@stud.ntnu.no>
2014-03-27 11:47:31 +0100

    Incomplete cleaning for Brat

--- brat_format.py
+++ brat_format.py
@@ -5,9 +5,19 @@ its own line).
 
 Currently defined for PLoS format -> Brat format only.
 """
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 import xml.etree.ElementTree as ET
 import os
+
+def convert_all_plos_to_brat(txtfolder="TXT", sofolder="SO", bratfolder="Brat", corenlpfolder="CoreNLP"):
+    """
+        Converts all files found in txtfolder to Brat format. Does not check for mismatch (i.e. lack of
+        parsed xml for a given txt etc)
+    """
+    for filename_full in os.listdir(txtfolder):
+        filename = filename_full[:filename_full.index('.')]
+        convert_plos_to_brat(filename, txtfolder=txtfolder, sofolder=sofolder, bratfolder=bratfolder, corenlpfolder=corenlpfolder)
+    
 
 def convert_plos_to_brat(filename, txtfolder="TXT", sofolder="SO", bratfolder="Brat", corenlpfolder="CoreNLP"):
     """
@@ -16,7 +26,7 @@ def convert_plos_to_brat(filename, txtfolder="TXT", sofolder="SO", bratfolder="B
     if not os.path.isdir(bratfolder): os.mkdir(bratfolder)
     
     # Store the scopes of allthe interesting mark-up tags
-    useful_tags = ['abstract', 'body', 'title', 'fig', 'supplementary-material', 'table-wrap']
+    useful_tags = ['abstract', 'body', 'title', 'fig', 'table-wrap']
     scopes = []
     with open(os.path.join(sofolder, filename+'.so'), 'r') as file:
         for line in file:
@@ -45,7 +55,7 @@ def convert_plos_to_brat(filename, txtfolder="TXT", sofolder="SO", bratfolder="B
             sentence_text.append(token.find('word').text)
         # Store only sentences that are in the abstract or body
         tags = [tag for tag, start, end in scopes if startoffset>=start and endoffset<=end]
-        if ('abstract' in tags or 'body' in tags) and not 'fig' in tags and not 'supplementary-material' in tags and not 'table-wrap' in tags:
+        if ('abstract' in tags or 'body' in tags) and not 'fig' in tags and not 'table-wrap' in tags:
             # Remove all parts of section titles from the sentence. Stanford NLP tends to mix these into sentences.
             # Assumes, as seems to be the case in CoreNLP, that titles are only attached to the start of sentences.
             for tag, start, end in scopes:
@@ -61,14 +71,22 @@ def convert_plos_to_brat(filename, txtfolder="TXT", sofolder="SO", bratfolder="B
 
 if __name__ == "__main__":
     optparser = OptionParser(description="Script to convert parsed text to Brat format.")
-    optparser.add_option("-f", "--filename", default=None, dest="filename", help="The filename of the file to be converted.")
+
     optparser.add_option("-t", "--txtfolder", default="TXT", dest="txtfolder", help="Name of the folder that contains the .txt files output by NXML2TXT.")
     optparser.add_option("-s", "--sofolder", default="SO", dest="sofolder", help="Name of the folder that contains the .so files output by NXML2TXT.")
     optparser.add_option("-c", "--corenlpfolder", default="CoreNLP", dest="corenlpfolder", help="Name of the folder that contains the .xml files output by Stanford CoreNLP.")
     optparser.add_option("-b", "--bratfolder", default="Brat", dest="bratfolder", help="Output folder.")
+    
+    group = OptionGroup(optparser, "File selection options")
+    group.add_option("-f", "--filename", default=None, dest="filename", help="The filename of the file to be converted to convert a single file.")
+    group.add_option("-a", "--all", default=False, dest="all_files", action="store_true", help="Pass this instead of a filename to convert all files found by the program.")    
+    optparser.add_option_group(group)
+    
     (options, args) = optparser.parse_args()
     
-    filename = options.filename
-    assert filename, "You must specify which file to convert!"
+    assert options.filename or options.all_files, "You must specify eith one file to convert, or choose to convert all!"
     
-    convert_plos_to_brat(filename, txtfolder=options.txtfolder, sofolder=options.sofolder, bratfolder=options.bratfolder, corenlpfolder=options.corenlpfolder)
+    if options.all_files:
+        convert_all_plos_to_brat(txtfolder=options.txtfolder, sofolder=options.sofolder, bratfolder=options.bratfolder, corenlpfolder=options.corenlpfolder)
+    else:
+        convert_plos_to_brat(options.filename, txtfolder=options.txtfolder, sofolder=options.sofolder, bratfolder=options.bratfolder, corenlpfolder=options.corenlpfolder)
